cf108ee066fceb11540b2e6450a262edd4d0e736
Toby Fee <toby.fee@nwea.org>
2016-10-17 15:09:24 -0700

    arduino version without calling delay()

---
+++ arduino/probeControl/probeControl.ino
@@ -0,0 +1,92 @@
+
+const int stopButtonPin = 4;     
+const int goButtonPin = 2;
+const int stopLedPin =  8;
+const int goLedPin =  12;
+const int backwardLedPin =  13;
+const int forwardLedPin =  7;
+const int buttonThreshold = 1000; //how many frames we need to see the button pressed to trigger
+const int stepInterval = 400;
+const int blinkInterval = stepInterval/2;
+
+int movesArray[] = {10, -5, -5}; //the in, out, out steps
+int moving = 0; //are we in the act of completing a move
+int moveIndex = 0; //the index in the array we should be on
+
+int stopButtonCounter = 0;
+int goButtonCounter = 0;
+int stopButtonState = 0;
+int goButtonState = 0;
+unsigned long previousMillis = 0;
+
+
+
+void move(int index) 
+  {
+    if (movesArray[index] > 0 ){
+
+      }
+  }
+
+void setup() {
+  // initialize the LEDs pin as an output:
+  pinMode(stopLedPin, OUTPUT);
+  pinMode(goLedPin, OUTPUT);
+  pinMode(forwardLedPin, OUTPUT);
+
+
+  // initialize the pushbuttons pin as an input:
+  pinMode(stopButtonPin, INPUT);
+  pinMode(goButtonPin, INPUT);
+}
+
+void handleInputs()
+  {
+    //track state for the 'stop' button
+    if (digitalRead(stopButtonPin) == 1){
+      stopButtonCounter++;
+      if (stopButtonCounter > buttonThreshold){
+          stopButtonState = 1;
+        }
+      } 
+      else 
+      {
+        stopButtonCounter = 0; 
+        stopButtonState = 0;
+      }
+    //track state for the 'stop' button
+    
+    if (digitalRead(goButtonPin) == 1){
+      goButtonCounter++;
+      if (goButtonCounter > buttonThreshold && stopButtonState == 0){
+          goButtonState = 1;
+        }
+      } 
+      else 
+      {
+        goButtonCounter = 0; 
+        goButtonState = 0;
+      }
+  }
+
+
+void loop() {
+  unsigned long currentMillis = millis();
+       
+   // How much time has passed, accounting for rollover with subtraction!
+   if ((unsigned long)(currentMillis - previousMillis) >= blinkInterval && moving) {
+      digitalWrite(forwardLedPin, !digitalRead(forwardLedPin)); // Toggle the LED
+       previousMillis = currentMillis;
+   }
+  handleInputs();
+  digitalWrite(stopLedPin, stopButtonState);
+  digitalWrite(goLedPin, goButtonState);
+    if (goButtonState == 1)
+    {
+     moving = 1;
+     } else if (stopButtonState == 1)
+    {
+      moving = 0;
+      digitalWrite(forwardLedPin, 0);
+    }
+}
