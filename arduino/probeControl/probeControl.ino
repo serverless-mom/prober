21aeb714586e6540f48acaa099d1a26be5f0360e
Toby Fee <toby.fee@nwea.org>
2016-10-19 14:06:37 -0700

    working sequencing, and stopping starting buttons. System gets in a weird state after completing its sequence, so I need to work on a cleanup at the end.

--- arduino/probeControl/probeControl.ino
+++ arduino/probeControl/probeControl.ino
@@ -1,3 +1,4 @@
+#include <SoftwareSerial.h>
 
 const int stopButtonPin = 4;     
 const int goButtonPin = 2;
@@ -6,26 +7,85 @@ const int goLedPin =  12;
 const int backwardLedPin =  13;
 const int forwardLedPin =  7;
 const int buttonThreshold = 1000; //how many frames we need to see the button pressed to trigger
-const int stepInterval = 400;
-const int blinkInterval = stepInterval/2;
+const int stepInterval = 449;
+const int blinkInterval = 47;
+const int displayInterval = 103;
+
+
+SoftwareSerial mySerial(3,6); // pin 3 = RX (unused), pin 2 = TX, 
 
 int movesArray[] = {10, -5, -5}; //the in, out, out steps
+const int movesCount = 3;
 int moving = 0; //are we in the act of completing a move
-int moveIndex = 0; //the index in the array we should be on
+int moveIndex = -1; //the index in the array we should be on
+int depth = 0;
+char depthString[10];
 
 int stopButtonCounter = 0;
 int goButtonCounter = 0;
 int stopButtonState = 0;
 int goButtonState = 0;
-unsigned long previousMillis = 0;
+unsigned long stepPreviousMillis = 0;
+unsigned long blueLedPreviousMillis = 0;
+unsigned long displayPreviousMillis = 0;
 
 
-
-void move(int index) 
+void clearDisplay()
   {
-    if (movesArray[index] > 0 ){
+    resetCursor();
+    mySerial.write("                "); // clear display
+    mySerial.write("                ");
+    resetCursor();
+    mySerial.write("Depth: ");
+  }
 
-      }
+void resetCursor()
+  {
+    mySerial.write(254); // move cursor to beginning of first line
+    mySerial.write(128);
+  }
+
+void updateDisplay()
+  {
+    mySerial.write(254);
+    mySerial.write(136);
+    sprintf(depthString,"%4d",depth);
+    mySerial.write(depthString);
+    mySerial.write(254);
+    mySerial.write(141);
+    if (moveIndex == -1 || movesArray[moveIndex] > 0){
+        mySerial.write('v');
+      } else {
+        mySerial.write('^');
+      }    
+  }
+
+
+void move() 
+  {
+    if (movesArray[moveIndex] > 0 ){
+      movesArray[moveIndex]--;
+      depth--;
+    } else if (movesArray[moveIndex] < 0 ){
+      movesArray[moveIndex]++;
+      depth++;
+    } else if (movesArray[moveIndex] == 0 ){
+      stopMove();
+    }
+  }
+  
+void stopMove()
+  {
+    digitalWrite(forwardLedPin, 0);
+    moving = 0;
+  }
+
+void startMove()
+  {
+    if (moveIndex == -1 || movesArray[moveIndex] == 0){
+      moveIndex++;
+    }
+    moving = 1;
   }
 
 void setup() {
@@ -33,11 +93,13 @@ void setup() {
   pinMode(stopLedPin, OUTPUT);
   pinMode(goLedPin, OUTPUT);
   pinMode(forwardLedPin, OUTPUT);
-
-
   // initialize the pushbuttons pin as an input:
   pinMode(stopButtonPin, INPUT);
   pinMode(goButtonPin, INPUT);
+  mySerial.begin(9600); // set up serial port for 9600 baud
+  delay(500); // wait for display to boot up
+  clearDisplay();
+  updateDisplay();
 }
 
 void handleInputs()
@@ -71,22 +133,35 @@ void handleInputs()
 
 
 void loop() {
-  unsigned long currentMillis = millis();
-       
-   // How much time has passed, accounting for rollover with subtraction!
-   if ((unsigned long)(currentMillis - previousMillis) >= blinkInterval && moving) {
-      digitalWrite(forwardLedPin, !digitalRead(forwardLedPin)); // Toggle the LED
-       previousMillis = currentMillis;
-   }
   handleInputs();
   digitalWrite(stopLedPin, stopButtonState);
   digitalWrite(goLedPin, goButtonState);
-    if (goButtonState == 1)
+    if (goButtonState == 1 && moving == 0)
     {
-     moving = 1;
-     } else if (stopButtonState == 1)
+      startMove();
+    } 
+    else if (stopButtonState == 1)
     {
-      moving = 0;
-      digitalWrite(forwardLedPin, 0);
+      stopMove();
     }
+
+  
+  unsigned long currentMillis = millis();
+       
+   // How much time has passed, accounting for rollover with subtraction!
+   if ((unsigned long)(currentMillis - blueLedPreviousMillis) >= blinkInterval && moving) {
+      digitalWrite(forwardLedPin, !digitalRead(forwardLedPin)); // Toggle the LED
+       blueLedPreviousMillis = currentMillis;
+   }
+   if ((unsigned long)(currentMillis - displayPreviousMillis) >= displayInterval) {
+    updateDisplay();
+    displayPreviousMillis = currentMillis;
+   }
+   if ((unsigned long)(currentMillis - stepPreviousMillis) >= stepInterval && moving && moveIndex < movesCount ) {
+    move();
+    stepPreviousMillis = currentMillis;
+   }
+   
+
+
 }
